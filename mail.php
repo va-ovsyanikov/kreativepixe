90709e7d38878969cd60922d7e3b35843c026acc
Vitalka <va.ovsyanikov@gmail.com>
2016-04-28 19:08:39 +0300

    first commit

--- mail.php
+++ mail.php
@@ -1,55 +1,55 @@
-﻿<?php
+<?php
 $name=$_POST["name"];
 $email=$_POST["email"];
 $mess=$_POST["mess"];
 
 if (isset ($name))
 {
-$name = substr($name,0,20); //Не может быть более 20 символов
-if (empty($name))
-{
-echo "<center><b>Не указано имя !!!<p>";
-echo "<a href=index.html>Вернуться и правильно заполнить форму.</a>";
-exit;
-}
+    $name = substr($name,0,20); //Не может быть более 20 символов
+    if (empty($name))
+    {
+        echo "<center><b>Не указано имя !!!<p>";
+        echo "<a href=index.html>Вернуться и правильно заполнить форму.</a>";
+        exit;
+    }
 }
 else
 {
-$name = "не указано";
+    $name = "не указано";
 }
 if (isset ($email))
 {
-$email = substr($email,0,20); //Не может быть более 20 символов
-if (empty($email))
-{
-echo "<center><b>Не указан e-mail !!!<p>";
-echo "<a href=index.php>Вернуться и правильно заполнить форму.</a>";
-exit;
-}
+    $email = substr($email,0,20); //Не может быть более 20 символов
+    if (empty($email))
+    {
+        echo "<center><b>Не указан e-mail !!!<p>";
+        echo "<a href=index.php>Вернуться и правильно заполнить форму.</a>";
+        exit;
+    }
 }
 else
 {
-$email = "не указано";
+    $email = "не указано";
 }
 if (isset ($mess))
 {
-$mess = substr($mess,0,1000); //Не может быть более 1000 символов
-if (empty($mess))
-{
-echo "<center><b>Сообщение не написано !!!<p>";
-echo "<a href=index.html>Вернуться и правильно заполнить форму.</a>";
-exit;
-}
+    $mess = substr($mess,0,1000); //Не может быть более 1000 символов
+    if (empty($mess))
+    {
+        echo "<center><b>Сообщение не написано !!!<p>";
+        echo "<a href=index.html>Вернуться и правильно заполнить форму.</a>";
+        exit;
+    }
 }
 else
 {
-$mess = "не указано";
+    $mess = "не указано";
 }
 $i = "не указано";
 if ($name == $i AND $email == $i AND $mess == $i)
 {
-echo "Ошибка ! Скрипту не были переданы параметры !";
-exit;
+    echo "Ошибка ! Скрипту не были переданы параметры !";
+    exit;
 }
 $to = "va.ovsyanikov@gmail.com";
 $subject = "Сообщение с вашего интернет-сайта";
